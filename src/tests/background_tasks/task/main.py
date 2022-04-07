eb7ffe03e8fee0440a8f31dd504b3fde8c3dc529
Teag John <teagjohnbp@gmail.com>
2022-04-07 15:38:14 +0700

    init

--- src/tests/background_tasks/task/main.py
+++ src/tests/background_tasks/task/main.py
@@ -1,6 +1,5 @@
 import unittest
 
-
 class TestTaskOrmEntity(unittest.TestCase):
     def test_initialize(self):
         self.assertEqual(15, 15)
@@ -28,21 +27,45 @@ class TestTasktResultRepository(unittest.TestCase):
     def testDemo(self):
         assert True
 
-
+# task/domain/entities/task.py
 class TestTaskProps(unittest.TestCase):
     def testDemo(self):
         assert True
 
+    def test_validate(self):
+        assert True
+
+# task/domain/entities/task.py
 class TestTaskEntity(unittest.TestCase):
     def testDemo(self):
         assert True
 
+    def test_props_klass(self):
+        assert True
+
+# task/domain/entities/task_result.py
 class TestTaskResultProps(unittest.TestCase):
     def testDemo(self):
         assert True
 
+    def test_real_file_path(self):
+        assert True
+
+# task/domain/entities/task_result.py
 class TestTaskResultEntity(unittest.TestCase):
     def testDemo(self):
+        assert True
+
+    def test_props_klass(self):
+        assert True
+
+    async def test_save_request_result_to_file(self):
+        assert True
+
+    async def test_read_data_from_file(self):
+        assert True
+
+    def test_check_if_file_exists(self):
         assert True
 
 if __name__ == '__main__':
