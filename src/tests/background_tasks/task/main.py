5d5d3f66aa3404c9de6e6f85266ffac4e2a2236c
Teag John <teagjohnbp@gmail.com>
2022-04-07 15:24:41 +0700

    init

---
+++ src/tests/background_tasks/task/main.py
@@ -0,0 +1,49 @@
+import unittest
+
+
+class TestTaskOrmEntity(unittest.TestCase):
+    def test_initialize(self):
+        self.assertEqual(15, 15)
+
+
+class TestTaskOrmMapper(unittest.TestCase):
+    def testDemo(self):
+        assert True
+
+
+class TestTaskRepository(unittest.TestCase):
+    def testDemo(self):
+        assert True
+
+
+class TestTaskResultOrmEntity(unittest.TestCase):
+    def testDemo(self):
+        assert True
+
+class TestTaskResultOrmMapper(unittest.TestCase):
+    def testDemo(self):
+        assert True
+
+class TestTasktResultRepository(unittest.TestCase):
+    def testDemo(self):
+        assert True
+
+
+class TestTaskProps(unittest.TestCase):
+    def testDemo(self):
+        assert True
+
+class TestTaskEntity(unittest.TestCase):
+    def testDemo(self):
+        assert True
+
+class TestTaskResultProps(unittest.TestCase):
+    def testDemo(self):
+        assert True
+
+class TestTaskResultEntity(unittest.TestCase):
+    def testDemo(self):
+        assert True
+
+if __name__ == '__main__':
+    unittest.main()
