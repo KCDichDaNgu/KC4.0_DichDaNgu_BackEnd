8bb90f167110c268fd417af1882898cf57938437
kienle371999 <kienle371999@gmail.com>
2022-04-11 10:19:35 +0700

    add unit-tests for detect_file_language

---
+++ src/tests/background_tasks/detect_file_language_created_by_private_request/READEME.md
@@ -0,0 +1,8 @@
+## Hướng dẫn cách thực hiện kiểm thử cho hàm `detect_file_language_created_by_private_request`
+
+- Đăng nhập vào hệ thống
+- Thực hiện cách yêu cầu dịch liên quan đến văn bản
+- Vào database để xem những yêu cầu dịch với trạng thái `not_yet_processed`
+- Sao chép phần `task_id`, `created_id` vào file `detect_file_language_data.csv` theo thứ tự `not_yet_processed_task_id`, `created_id`
+- Chỉnh sửa file `tests/background_tasks/main.py` phù hợp 
+- Thực hiện command `python3 src/server.py run-server -p 8001` để tiến hành quá trình kiểm thử  và quan sát kết quả
---
+++ src/tests/background_tasks/detect_file_language_created_by_private_request/__init__.py
@@ -0,0 +1,19 @@
+
+
+def add_fresh_jobs(background_task_scheduler, BACKGROUND_TASKS):
+    
+    from tests.background_tasks.detect_file_language_created_by_private_request.main import test_all
+    
+    background_task_conf = BACKGROUND_TASKS['detect_file_language_created_by_private_request.test_all']
+    
+    if background_task_scheduler.get_job(background_task_conf.ID) != None:
+        background_task_scheduler.remove_job(background_task_conf.ID)
+ 
+    background_task_scheduler.add_job(
+        test_all,
+        id=background_task_conf.ID,
+        trigger=background_task_conf.TRIGGER,
+        **background_task_conf.CONFIG
+    )
+    
+    return background_task_scheduler
